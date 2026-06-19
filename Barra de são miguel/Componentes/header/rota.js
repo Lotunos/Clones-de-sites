167ada79a127e37764dd6e42f83cf41e39bd42c7
José Álvaro Coelho Alves da Costa <jose-alvaro-coelho@hotmail.com>
2026-06-19 11:06:59 -0300

    Ajuste no componente
    Ajuste no componente

--- Barra de são miguel/Componentes/header/rota.js
+++ Barra de são miguel/Componentes/header/rota.js
@@ -5,11 +5,21 @@ function rota() {
     let htmlGerado = `<span><a href="/"></a></span>`;
     let caminhoAcumulado = '';
     partes.forEach((parte, index) => {
-        caminhoAcumulado += `/${parte}`;
-        let nomeFormatado = decodeURIComponent(parte).replace(/[-_]/g, ' ');
-        nomeFormatado = nomeFormatado.charAt(0).toUpperCase() + nomeFormatado.slice(1) + "/";
+    caminhoAcumulado += `/${parte}`;
+    
+    // Decodifica a URL e troca traços/underlines por espaços
+    let nomeFormatado = decodeURIComponent(parte).replace(/[-_]/g, ' ');
+
+    // Compara em minúsculo para evitar erros se a URL variar
+    if (nomeFormatado.toLowerCase() !== 'barra de são miguel') {
+        
+        // Capitaliza a primeira letra e adiciona a barra no final
+        nomeFormatado = "/"+nomeFormatado.charAt(0).toUpperCase() + nomeFormatado.slice(1) + "/";
+        
+        // Alimenta a string do HTML
         htmlGerado += `<span><a href="${caminhoAcumulado}">${nomeFormatado}</a></span>`;
-    });
+    }
+});
 
     caminho.innerHTML = htmlGerado;
 }
