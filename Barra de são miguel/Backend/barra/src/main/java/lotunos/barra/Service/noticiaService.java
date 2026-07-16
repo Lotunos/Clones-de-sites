2e684e0ac7d75008c46884bf6399a4791ed78601
José Álvaro Coelho Alves da Costa <jose-alvaro-coelho@hotmail.com>
2026-07-16 17:04:09 -0300

    o metodo criarNoticia provisório

--- Barra de são miguel/Backend/barra/src/main/java/lotunos/barra/Service/noticiaService.java
+++ Barra de são miguel/Backend/barra/src/main/java/lotunos/barra/Service/noticiaService.java
@@ -1,22 +1,42 @@
 package lotunos.barra.Service;
 
+import jakarta.transaction.Transactional;
 import lotunos.barra.DAO.noticiaDAO;
 import lotunos.barra.DTO.noticiaDTO;
 import lotunos.barra.Model.noticiaModel;
+import lotunos.barra.suporte.uploadService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Service
 public class noticiaService {
 
     private noticiaDAO noticiaDAO;
-    public noticiaService(noticiaDAO noticiaDAO){
+    private uploadService upload;
+    public noticiaService(noticiaDAO noticiaDAO, uploadService upload){
         this.noticiaDAO = noticiaDAO;
+        this.upload = upload;
     }
-    public void criarNoticia(noticiaDTO dto){
+    @Transactional
+    public void criarNoticia(noticiaDTO dto, MultipartFile arquivo) throws IOException {
+        Path caminho = null;
         noticiaModel noticia = new noticiaModel();
-        noticia.setNome_Arquivo(dto.getNome_Arquivo());
-        noticia.setLocal(dto.getLocal());
-        noticiaDAO.save(noticia);
+        try {
+            caminho = upload.salvarImagem(arquivo,"noticia",dto.getLocal());
+            noticia.setNome_Arquivo(dto.getNome_Arquivo());
+            noticia.setLocal(dto.getLocal());
+            noticia.setCaminhoArquivo(dto.getCaminhoArquivo());
+            noticiaDAO.save(noticia);
+        } catch(Exception e){
+                if (caminho != null) {
+                    Files.deleteIfExists(caminho);
+                }
+
+        }
     }
 
 }
